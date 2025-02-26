4d0bada383aff4624e0d495beb71cb557f3d92a9
kacper-sx <kacper.saweczko@o2.pl>
2025-02-26 16:05:13 +0100

    feat: about section changes

--- src/components/features/Numbers.tsx
+++ src/components/features/Numbers.tsx
@@ -4,8 +4,8 @@ import CountUp from "react-countup"
 
 const stats = [
   { value: 15, label: "lat na rynku" },
-  { value: 80, label: "klimatyzacji w roku" },
-  { value: 50, label: "pomp ciepła w roku" },
+  { value: 80, label: "montaży klimatyzacji w roku" },
+  { value: 50, label: "montaży pomp ciepła w roku" },
   { value: "∞", label: "zadowolonych klientów", noPlus: true },
 ]
 
@@ -23,9 +23,9 @@ const AnimatedNumbers = () => {
   }, [])
 
   return (
-    <div className="flex bg-bbrand-50 py-8 px-12 flex-col gap-8 items-center xl:flex-row rounded-3xl justify-center">
+    <div className="flex bg-bbrand-50 py-8 px-12 flex-col gap-8 items-center xl:flex-row rounded-3xl justify-center shadow-2xl shadow-accent">
       {stats.map(({ value, label, noPlus }, index) => (
-        <div key={index} ref={index === 0 ? ref : null} className="flex flex-col gap-2 xl:gap-4 items-center xl:pr-8 xl:border-r border-border last:border-r-0">
+        <div key={index} ref={index === 0 ? ref : null} className="flex flex-col gap-2 xl:gap-4 items-center xl:pr-8 last:pr-0 xl:border-r border-border last:border-r-0">
           <span className="font-extrabold text-accent text-3xl">
             {typeof value === "number" && isInView ? <CountUp duration={10} start={0} end={value} /> : value} {noPlus ? "" : "+"}
           </span>
